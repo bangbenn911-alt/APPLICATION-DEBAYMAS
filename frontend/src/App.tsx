92b338e40e115023b07b456a99e66cba0227114e
BENS <bangbenn911-alt>
2026-09-07 07:35:01 +0700

    update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import CandidateDashboard from './pages/CandidateDashboard';
-import AssessmentRunner from './components/AssessmentRunner';
+import { Login } from './pages/Login';
+import { CandidateDashboard } from './pages/CandidateDashboard';
+import { AssessmentRunner } from './components/AssessmentRunner';
 
 function App() {
   return (
